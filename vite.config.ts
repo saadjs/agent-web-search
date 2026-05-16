b6e78fc8e1fe82b832365e34144da544fa75e805
Saad Bash <skbash89@gmail.com>
2026-05-15 22:48:43 -0400

    chore: migrate app to vite router

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from "vite"
-import { devtools } from "@tanstack/devtools-vite"
-import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import viteTsConfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
-import { nitro } from "nitro/vite"
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
-    devtools(),
-    nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
+    viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
-    tanstackStart(),
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     viteReact(),
   ],
+  server: { port: 3000 },
 })
-
-export default config
